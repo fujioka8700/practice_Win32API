9377971614df4947da2e75eeadb095da238afc45
Masahiro Fujioka <fujioka8700@gmail.com>
2024-02-26 20:12:20 +0900

    [change] 複数の項目を指定する

--- practice_Win32API/main.cpp
+++ practice_Win32API/main.cpp
@@ -5,7 +5,7 @@ int WINAPI WinMain(
 	_In_opt_  HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine,
 	_In_ int nShowCmd) {
-	MessageBox(NULL, TEXT("Kitty on your lap"), TEXT("メッセージボックス"), MB_OK);
+	MessageBox(NULL, TEXT("Kitty on your lap"), TEXT("メッセージボックス"), MB_OK | MB_ICONINFORMATION);
 
 	return 0;
 }
