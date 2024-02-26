5052c32a0b8890cba6373240c02949b88ef28bba
Masahiro Fujioka <fujioka8700@gmail.com>
2024-02-26 20:22:56 +0900

    [change] 疑問符のアイコンにする

--- practice_Win32API/main.cpp
+++ practice_Win32API/main.cpp
@@ -5,7 +5,19 @@ int WINAPI WinMain(
 	_In_opt_  HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine,
 	_In_ int nShowCmd) {
-	MessageBox(NULL, TEXT("Kitty on your lap"), TEXT("メッセージボックス"), MB_OK | MB_ICONINFORMATION);
+
+	int on_button;
+
+	on_button = MessageBox(NULL, TEXT("Do you like Nekomimi?"),
+		TEXT("質問しています"), MB_YESNO | MB_ICONQUESTION);
+
+	if (on_button == IDYES) {
+		MessageBox(NULL, TEXT("You are nice!"),
+			TEXT("メッセージボックス"), MB_OK);
+	} else {
+		MessageBox(NULL, TEXT("Be Killed!"),
+			TEXT("メッセージボックス"), MB_OK);
+	}
 
 	return 0;
 }
