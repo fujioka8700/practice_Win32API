9a05539d67d47f6e85541498871aaaabf97a319e
Masahiro Fujioka <fujioka8700@gmail.com>
2024-02-26 20:07:31 +0900

    [update] cahte text.

--- practice_Win32API/main.cpp
+++ practice_Win32API/main.cpp
@@ -1,13 +1,11 @@
-#define STRICT
 #include <windows.h>
-#include <tchar.h>
 
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_  HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine,
 	_In_ int nShowCmd) {
-	MessageBox(NULL, _T("処理は成功しました。"), _T("情報"), MB_OK | MB_ICONINFORMATION);
+	MessageBox(NULL, TEXT("Kitty on your lap"), TEXT("メッセージボックス"), MB_OK);
 
 	return 0;
 }
