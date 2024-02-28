4340e2fd06f222fbe5655c381af5a72bac5aa97c
Masahiro Fujioka <fujioka8700@gmail.com>
2024-02-28 19:59:42 +0900

    [change] lstrcpyn

--- practice_Win32API/main.cpp
+++ practice_Win32API/main.cpp
@@ -10,8 +10,7 @@ int WINAPI WinMain(
 	CONST TCHAR* str1 = _T("Kitty on your lap");
 	TCHAR str2[32];
 
-	lstrcpy(str2, str1);
-
+	lstrcpyn(str2, str1, lstrlen(str1) + 1);
 	MessageBox(NULL, str2, str2, MB_OK);
 
 	return 0;
