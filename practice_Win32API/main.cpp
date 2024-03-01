ddda1b0a6b9d79344e620995676dda2eb59eaf73
Masahiro Fujioka <fujioka8700@gmail.com>
2024-03-01 20:47:55 +0900

    [update] ウィンドウクラスの登録

--- practice_Win32API/main.cpp
+++ practice_Win32API/main.cpp
@@ -5,11 +5,23 @@ int WINAPI WinMain(
 	_In_opt_  HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine,
 	_In_ int nShowCmd) {
-
 	HWND hwnd;
+	WNDCLASS winc;
 
-	hwnd = CreateWindowEx(WS_EX_TOOLWINDOW,
-		TEXT("STATIC"), TEXT("Kitty on your lap"),
+	winc.style = CS_HREDRAW | CS_VREDRAW;
+	winc.lpfnWndProc = DefWindowProc;
+	winc.cbClsExtra = winc.cbWndExtra = 0;
+	winc.hInstance = hInstance;
+	winc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	winc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	winc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	winc.lpszMenuName = NULL;
+	winc.lpszClassName = TEXT("KITTY");
+
+	if (!RegisterClass(&winc)) return 0;
+
+	hwnd = CreateWindow(
+		TEXT("KITTY"), TEXT("Kitty on your lap"),
 		WS_OVERLAPPEDWINDOW,
 		100, 100, 200, 200, NULL, NULL,
 		hInstance, NULL
