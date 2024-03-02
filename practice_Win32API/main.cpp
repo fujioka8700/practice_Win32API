59a3a37c0acdab540d6908a55033adbb60d0fd0e
Masahiro Fujioka <fujioka8700@gmail.com>
2024-03-02 11:08:58 +0900

    [update] ウィンドウプロシージャ

--- practice_Win32API/main.cpp
+++ practice_Win32API/main.cpp
@@ -1,4 +1,14 @@
 #include <windows.h>
+#include<stdlib.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+	if (msg == WM_LBUTTONUP) {
+		MessageBox(hwnd, TEXT("èIÇÌÇÈÇ…Ç·ÇÒ"),
+			TEXT("Kitty"), MB_ICONINFORMATION);
+		exit(0);
+	}
+	return DefWindowProc(hwnd, msg, wp, lp);
+}
 
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
@@ -7,10 +17,23 @@ int WINAPI WinMain(
 	_In_ int nShowCmd) {
 	HWND hwnd;
 	MSG msg;
+	WNDCLASS winc;
+
+	winc.style = CS_HREDRAW | CS_VREDRAW;
+	winc.lpfnWndProc = WndProc;
+	winc.cbClsExtra = winc.cbWndExtra = 0;
+	winc.hInstance = hInstance;
+	winc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	winc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	winc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	winc.lpszMenuName = NULL;
+	winc.lpszClassName = TEXT("KITTY");
+
+	if (!RegisterClass(&winc)) return 0;
 
 	hwnd = CreateWindow(
-		TEXT("BUTTON"), TEXT("Kitty on your lap"),
-		WS_CAPTION | WS_VISIBLE,
+		TEXT("KITTY"), TEXT("Kitty on your lap"),
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
 		100, 100, 200, 200, NULL, NULL,
 		hInstance, NULL
 	);
@@ -19,7 +42,6 @@ int WINAPI WinMain(
 
 	while (TRUE) {
 		GetMessage(&msg, NULL, 0, 0);
-		if (msg.message == WM_LBUTTONUP) break;
 		DispatchMessage(&msg);
 	}
 	return 0;
