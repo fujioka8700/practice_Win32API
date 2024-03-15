1543fe3447800cf60143d08ef7a6269f4dccaaa4
Masahiro Fujioka <fujioka8700@gmail.com>
2024-03-15 20:50:59 +0900

    [update] KillTimer

--- practice_Win32API/main.cpp
+++ practice_Win32API/main.cpp
@@ -1,26 +1,28 @@
 #include <windows.h>
 
-VOID CALLBACK TimerProc(HWND hwnd, UINT uMsg, UINT idEvent, DWORD dwTime) {
-	HDC hdc;
-	static int iCount = 0;
-	TCHAR strCount[64];
-
-	iCount++;
-
-	hdc = GetDC(hwnd);
-	wsprintf(strCount, "%d", iCount);
-	TextOut(hdc, 10, 10, strCount, lstrlen(strCount));
-	ReleaseDC(hwnd, hdc);
-}
-
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	static int iCount = 0;
+	static TCHAR strCount[64];
+
 	switch (msg) {
 		case WM_DESTROY:
 			PostQuitMessage(0);
 			return 0;
 		case WM_CREATE:
-			SetTimer(hwnd, 1, 100, (TIMERPROC)TimerProc);
+			SetTimer(hwnd, 1, 100, NULL);
 			return 0;
+		case WM_TIMER:
+			iCount++;
+			if (iCount == 50) KillTimer(hwnd, 1);
+			InvalidateRect(hwnd, NULL, TRUE);
+			return 0;
+		case WM_PAINT:
+			hdc = BeginPaint(hwnd, &ps);
+			wsprintf(strCount, "%d", iCount);
+			TextOut(hdc, 10, 10, strCount, lstrlen(strCount));
+			EndPaint(hwnd, &ps);
 	}
 	return DefWindowProc(hwnd, msg, wp, lp);
 }
